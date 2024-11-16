d481a16a21f9b93bc7c30f6f88656a207ef37b5d
Ajay Theetharappan <ajaytheetharappan@gmail.com>
2024-11-16 12:30:28 +0700

    Integrated privy for login and logout flow

---
+++ fundnforget-react/src/context/InvestorContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const InvestorContext = createContext();
+
+export const InvestorProvider = ({ children }) => {
+  const [isInvestor, setIsInvestor] = useState(true);
+
+  return (
+    <InvestorContext.Provider value={{ isInvestor, setIsInvestor }}>
+      {children}
+    </InvestorContext.Provider>
+  );
+};
+
+export const useInvestor = () => useContext(InvestorContext);
