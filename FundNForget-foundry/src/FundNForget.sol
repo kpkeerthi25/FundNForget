310b573ef4088cc82054d7234c1a7a3b7ab02aa2
kpkeerthi <kpkeerthi34@gmail.com>
2024-11-17 07:37:13 +0700

    integrated pyth feed to compute initial investment for PnL calculation

--- FundNForget-foundry/src/FundNForget.sol
+++ FundNForget-foundry/src/FundNForget.sol
@@ -30,10 +30,10 @@ contract FundNForget is ReentrancyGuard, IEntropyConsumer {
         entropy = IEntropy(0x41c9e39574F40Ad34c79f1C99B66A45eFB830d4c);
         provider = entropy.getDefaultProvider();
         
-        // base-eth
-        pythNetworkMapping[0x5dEaC602762362FE5f135FA5904351916053cF70] = 0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a;
-        //base-usdc
-        pythNetworkMapping[0x4200000000000000000000000000000000000006] = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
+        // base-Syneth
+        pythNetworkMapping[0xc0AF1790125acB557467b7d8c13555eC063b096c] = 0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a;
+        //base-Synusdt
+        pythNetworkMapping[0x5dde0A29E8C5E0F2f3657cd65f17f6eA2C91C3EB] = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
         //base-synthetic-uni
         //mocking uni erc-20 in base sepolia
         pythNetworkMapping[0x6021D8Cc4388f917fc75766dA67eC54A1b4e4Cc6] = 0x78d185a741d07edb3412b09008b7c5cfb9bbbd7d568bf00ba737b456ba171501;
@@ -214,13 +214,17 @@ contract FundNForget is ReentrancyGuard, IEntropyConsumer {
             IERC20 token = IERC20(initialInvestments[i].tokenAddress);
         }
 
+        for(uint256 i = 0; i < initialInvestments.length; i++) {
+            initialInvestmentValue = initialInvestmentValue + calculateTokenUSDPrice(initialInvestments[i].tokenAddress) * initialInvestments[i].value;
+        }
+
         StrategySubscription memory subscription = StrategySubscription(
             _subscriptionIdCounter,
             fundManagerToLatestAttestationMapping[fundManagerAddress],
             msg.sender,
             fundManagerAddress,
             initialInvestments,
-            0, //TODO
+            initialInvestmentValue, //TODO
             block.timestamp,
             block.timestamp,
             true
@@ -283,7 +287,7 @@ contract FundNForget is ReentrancyGuard, IEntropyConsumer {
         // Each price feed (e.g., ETH/USD) is identified by a price feed ID.
         // The complete list of feed IDs is available at https://pyth.network/developers/price-feed-ids
 
-        bytes32 priceFeedId = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace; // ETH/USD
+        bytes32 priceFeedId =  pythNetworkMapping[tokenContract];
         // PythStructs.Price memory price = pyth.getPriceNoOlderThan(priceFeedId, 600);
         PythStructs.Price memory price = pyth.getPriceUnsafe(priceFeedId);
 
